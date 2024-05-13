7ed942f5d8e13b98c49be2e1aed027bebb56dcff
Jasiel-Z <zS21013907@estudiantes.uv.mx>
2024-05-13 11:59:31 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const app = express()
+
+
+dotenv.config();
+
+app.get("/", (req,res) => {
+    res.send("Hola mundo desde el API de XKBAM")
+
+})
+
+app.listen(process.env.SERVER_PORT, () =>{
+    console.log(`Aplicación de ejemplo escuchando en el puerto ${process.env.SERVER_PORT}`)
+} )
